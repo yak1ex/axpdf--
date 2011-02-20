7fca0ccb2c6a60f4e8b8edabb8c67f8d8513b3cd
Yak! <yak_ex@mx.scn.tv>
2011-02-20 20:27:18 +0900

    Add utility algorithm.

--- parser.hpp
+++ parser.hpp
@@ -25,6 +25,45 @@
 
 #include "spirit_helper.hpp"
 
+namespace yak { namespace util {
+
+	template<typename InputIterator1, typename InputIterator2>
+	std::pair<InputIterator1, InputIterator2>
+	safe_mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while(first1 != last1 && first2 != last2 && *first1 == *first2) {
+			++first1; ++first2;
+		}
+		return std::make_pair(first1, first2);
+	}
+	template<typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
+	std::pair<InputIterator1, InputIterator2>
+	safe_mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred)
+	{
+		while(first1 != last1 && first2 != last2 && pred(*first1, *first2)) {
+			++first1; ++first2;
+		}
+		return std::make_pair(first1, first2);
+	}
+	template<typename InputIterator1, typename InputIterator2>
+	bool safe_equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+	{
+		while(first1 != last1 && first2 != last2 && *first1 == *first2) {
+			++first1; ++first2;
+		}
+		return first1 == last1 && first2 == last2;
+	}
+	template<typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
+	bool safe_equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, BinaryPredicate pred)
+	{
+		while(first1 != last1 && first2 != last2 && pred(*first1, *first2)) {
+			++first1; ++first2;
+		}
+		return first1 == last1 && first2 == last2;
+	}
+
+}}
+
 namespace boost {
 	struct recursive_variant_ {};
 }
